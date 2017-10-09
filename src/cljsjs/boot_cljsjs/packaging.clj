ab441db6157ab33898cea6af0d0a73c0c6d0b811
Juho Teperi <teeepeli@gmail.com>
2017-10-09 21:18:11 +0300

    Update packaging.clj

--- src/cljsjs/boot_cljsjs/packaging.clj
+++ src/cljsjs/boot_cljsjs/packaging.clj
@@ -250,4 +250,5 @@
                   (throw (ex-info "Checksums do not match" {})))))
             (util/info "Checksums match\n"))
           (spit checksums-file (with-out-str (pprint/pprint new-checksums)))
+          (util/info "Checksum file (boot-cljsjs-checksums.edn) updated, please commit this file to Git.\n")
           (next-handler fileset))))))
